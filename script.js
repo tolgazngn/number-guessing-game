8d61ad86269bee70edad2d32de63d734625af6a5
Tolga <tolga79753@gmail.com>
2025-11-18 15:29:19 +0300

    chore: small adjustments

--- script.js
+++ script.js
@@ -1,69 +1,69 @@
 "use strict";
 
 const MAX_ATTEMPTS = 5;
-let randomNumber;
 
 //main
 
 showWelcomeMessage();
 
 while (true) {
-    playGame();
+  playGame();
 
-    let playAgain = confirm("Do you want to play again?");
+  let playAgain = confirm("Do you want to play again?");
 
-    if (!playAgain) break;
+  if (!playAgain) break;
 }
 
 //functions
 function showWelcomeMessage() {
-    alert(`Welcome to Number Guessing Game!\nYou have just ${MAX_ATTEMPTS} attempts.`);
+  alert(`Welcome to Number Guessing Game!\nYou have just ${MAX_ATTEMPTS} attempts.`);
 }
 
 function createRandomNumber() {
-    randomNumber = Math.floor(Math.random() * 100) + 1;
+  return Math.floor(Math.random() * 100) + 1;
 }
 
 function playGame() {
-    createRandomNumber();
+  let randomNumber = createRandomNumber();
 
-    let won = false;
+  let won = false;
+  let attemptsLeft = MAX_ATTEMPTS;
 
-    inner:
-    for (let attemptsLeft = MAX_ATTEMPTS; attemptsLeft > 0; attemptsLeft--) {
-        let guess = +prompt(`The number must between 1-100. Left ${attemptsLeft} attempts.`, "");
+  inner:
+  while (attemptsLeft > 0) {
+    let guess = +prompt(`The number must between 1-100. Left ${attemptsLeft} attempts.`, "");
 
-        if (isInvalidInput(guess)) {
-            attemptsLeft++; 
-            continue inner;
-        };
+    if (isInvalidInput(guess)) {
+      continue inner;
+    };
 
-        if (isOutOfRange(guess)) {
-            attemptsLeft++;
-            alert("Number must be 1 and 100.");
-            continue inner;
-        }
-
-        if (guess == randomNumber) {
-            won = true;
-            alert(`Congratulations! You found it in ${MAX_ATTEMPTS - attemptsLeft + 1} tries!`);
-            break;
-        } else if (guess < randomNumber) {
-            alert(`Too low!`);
-        } else {
-            alert(`Too high!`);
-        }
+    if (isOutOfRange(guess)) {
+      alert("The number must be between 1 and 100.");
+      continue inner;
     }
 
-    if (won == false) { 
-      alert(`Sorry, the number was ${randomNumber}.`); 
+    attemptsLeft--;
+
+    if (guess == randomNumber) {
+      won = true;
+      alert(`Congratulations! You found it in ${MAX_ATTEMPTS - attemptsLeft + 1} tries!`);
+      break;
+    } else if (guess < randomNumber) {
+      alert(`Too low!`);
+    } else {
+      alert(`Too high!`);
     }
+  }
+
+  if (won == false) {
+    alert(`Sorry, the number was ${randomNumber}.`);
+  }
 }
 
 function isInvalidInput(value) {
-    return value == null || value === "" || isNaN(value);
+  return value == null || value.trim() === "" || isNaN(value);
 }
 
 function isOutOfRange(value) {
-    return value < 1 || value > 100;
+  return value < 1 || value > 100;
 }
